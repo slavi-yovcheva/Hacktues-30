d6874fa750e986f7e873d888a4f67abd87d54a07
JasminaIvanova <jasmina.j.ivanova@gmail.com>
2018-03-16 19:46:38 +0200

    Add question scene

---
+++ Bulgaria/Assets/AnswerButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerButton : MonoBehaviour {
+	public Button correctBtn;
+	public Button wrongBtn;
+	public int score = 0;
+	void Start(){
+		correctBtn.onClick.AddListener (onCorrectPressed);
+	}
+
+	public void onCorrectPressed(){
+		Debug.Log ("Верен отговор!");
+		score += 10;
+	}
+	public void onWrongPressed(){
+		Debug.Log ("Грешен отговор!");
+	}
+
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+
+}
