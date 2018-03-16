cc9c10eb50c3a77dfd5339c51dbfd21a89482431
Slavi Yovcheva <slavyanka.yovcheva03@gmail.com>
2018-03-16 18:54:17 +0200

    Varnenska i Burgaska oblast, butonite sa nevidimi

---
+++ Bulgaria/Assets/Scripts/Varna.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Varna : MonoBehaviour 
+{	public Button button;
+	// Use this for initialization
+	void Start () 
+	{
+		Button btn = button.GetComponent<Button> ();
+		btn.onClick.AddListener (TaskOnClick);
+
+	}
+
+	public void TaskOnClick () 
+	{
+		SceneManager.LoadScene ("Varna", LoadSceneMode.Single);
+	}
+}
