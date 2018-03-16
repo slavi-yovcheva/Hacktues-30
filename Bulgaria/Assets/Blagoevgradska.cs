b8c40dd2b12973d10e93277e51e17a5e4e152502
Slavi Yovcheva <slavyanka.yovcheva03@gmail.com>
2018-03-16 11:09:27 +0200

    Bulgarian map

---
+++ Bulgaria/Assets/Blagoevgradska.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class Blagoevgradska : MonoBehaviour 
+{		
+	public Sprite sprite1;
+	public SpriteRenderer spriteRenderer;
+	// Use this for initialization
+	void Start () 
+	{
+		Button btn = GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+	
+	// Update is called once per frame
+	void TaskOnClick()
+	{
+		spriteRenderer.sprite = sprite1;
+	}
+}
