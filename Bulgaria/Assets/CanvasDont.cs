bbc316277526eb323641e29d35202b070abb2697
JasminaIvanova <jasmina.j.ivanova@gmail.com>
2018-03-17 19:51:26 +0200

    otchitane na tochki

---
+++ Bulgaria/Assets/CanvasDont.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasDont : MonoBehaviour {
+	public static bool eSuzdadenno = false;
+	void Start () {
+		if (eSuzdadenno == false) {
+			DontDestroyOnLoad (transform.gameObject);
+			eSuzdadenno = true;
+		} 
+		else {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Bulgaria/Assets/ScorePoints.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorePoints : MonoBehaviour {
+
+	public int points = 0;
+
+}
