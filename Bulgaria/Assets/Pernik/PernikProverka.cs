880e7a77c99169ee8afc8e7836190b5e1cfd8f48
Slavi Yovcheva <slavyanka.yovcheva03@gmail.com>
2018-03-17 21:23:19 +0200

    Pernishka oblast + vuprosi

---
+++ Bulgaria/Assets/Pernik/PernikProverka.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PernikProverka : MonoBehaviour {
+
+	public Button button;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Button btn = button.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	// Update is called once per frame
+	public void TaskOnClick () 
+	{
+		SceneManager.LoadScene("Pernik.Vupros", LoadSceneMode.Single);
+	}
+}
