01f4aa6c79bb0dbb6fd6cfef9fa81bd46cc0953c
JasminaIvanova <jasmina.j.ivanova@gmail.com>
2018-03-17 11:13:57 +0200

    vuprosi kum testa i greshen i veren otg

--- Bulgaria/Assets/Scripts/AnswerButton.cs
+++ Bulgaria/Assets/Scripts/AnswerButton.cs
@@ -2,21 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class AnswerButton : MonoBehaviour {
 	public Button correctBtn;
 	public Button wrongBtn;
 	public int score = 0;
-	void Start(){
+	void Start()
+	{
 		correctBtn.onClick.AddListener (onCorrectPressed);
+		wrongBtn.onClick.AddListener (onWrongPressed);
 	}
 
-	public void onCorrectPressed(){
-		Debug.Log ("Верен отговор!");
-		score += 10;
+	public void onCorrectPressed()
+	{
+			SceneManager.LoadScene ("Veren.Otgovor", LoadSceneMode.Single);
 	}
-	public void onWrongPressed(){
-		Debug.Log ("Грешен отговор!");
+
+	public void onWrongPressed()
+	{
+		SceneManager.LoadScene ("Greshen.Otgovor", LoadSceneMode.Single);
 	}
 
 	// Use this for initialization
---
+++ Bulgaria/Assets/Scripts/Продължение.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Продължение : MonoBehaviour 
+{
+	public Button button;
+	// Use this for initialization
+	void Start () 
+	{
+		Button btn = button.GetComponent<Button> ();
+		btn.onClick.AddListener (TaskOnClick);
+
+	}
+	
+	// Update is called once per frame
+	void TaskOnClick () 
+	{
+		SceneManager.LoadScene ("Bulgaria", LoadSceneMode.Single);
+	}
+}
