e704785f3ba0386fd2aaeb1c74fdb075f95d7690
Slavi Yovcheva <slavyanka.yovcheva03@gmail.com>
2018-03-18 08:56:29 +0200

    Veliko Turnovo + vuprosi

---
+++ Bulgaria/Assets/Veliko Turnovo/VTurnovoProverka.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class VTurnovoProverka : MonoBehaviour 
+{
+
+	public Button button;
+
+	void Start () 
+	{
+		Button btn = button.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	public void TaskOnClick () 
+	{
+		SceneManager.LoadScene("VTurnovo.Vupros", LoadSceneMode.Single);
+	}
+}
---
+++ Bulgaria/Assets/Veliko Turnovo/VelikoTurnovo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class VelikoTurnovo : MonoBehaviour 
+{
+	public Button button;
+
+	void Start () 
+	{
+		Button btn = button.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	public void TaskOnClick () 
+	{
+		SceneManager.LoadScene("Veliko Turnovo", LoadSceneMode.Single);
+	}
+}
