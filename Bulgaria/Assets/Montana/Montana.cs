37e98e5cbaea63f3536bae74877fa630d15437f1
Slavi Yovcheva <slavyanka.yovcheva03@gmail.com>
2018-03-18 05:13:32 +0200

    Montana + vuprosi

---
+++ Bulgaria/Assets/Montana/Montana.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Montana : MonoBehaviour 
+{
+	public Button button;
+
+	void Start () 
+	{
+		Button btn = button.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	public void TaskOnClick () 
+	{
+		SceneManager.LoadScene("Montana", LoadSceneMode.Single);
+	}
+}
