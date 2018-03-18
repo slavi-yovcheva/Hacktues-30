d1f5e73292d97d32e9309c309c03d08db2a8e0b6
Slavi Yovcheva <slavyanka.yovcheva03@gmail.com>
2018-03-18 07:17:02 +0200

    Silstra + vuprosi

---
+++ Bulgaria/Assets/Silistra/Silistra.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Silistra : MonoBehaviour 
+{
+	public Button button;
+
+	void Start () 
+	{
+		Button btn = button.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	public void TaskOnClick () 
+	{
+		SceneManager.LoadScene("Silistra", LoadSceneMode.Single);
+	}
+}
