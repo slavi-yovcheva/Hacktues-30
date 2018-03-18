149286a5a4f2dbd0a39fae93a7ea8698ec61be3d
Slavi Yovcheva <slavyanka.yovcheva03@gmail.com>
2018-03-18 07:37:00 +0200

    Sliven + vuprosi

---
+++ Bulgaria/Assets/Sliven/SlivenProverka.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SlivenProverka : MonoBehaviour 
+{
+
+	public Button button;
+
+	void Start () 
+	{
+		Button btn = button.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	public void TaskOnClick () 
+	{
+		SceneManager.LoadScene("Sliven.Vupros", LoadSceneMode.Single);
+	}
+}
